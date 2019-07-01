43ab16d80ce0a1b1e145988f08a67f802955bc32
Nurbek <38718409+Nurbek11@users.noreply.github.com>
2019-07-01 22:34:05 +0600

    Add files via upload

--- NumberOfGrain.java
+++ NumberOfGrain.java
@@ -1,16 +1,18 @@
 package BrainStorm;
 
+import java.math.BigDecimal;
+
 public class NumberOfGrain {
 
     public static void main(String[] args) {
         System.out.println(numOfGrain(64));
     }
 
-    static double numOfGrain(int numberOfCells) {
-        double numberOfGrain = 1;
+    static BigDecimal numOfGrain(int numberOfCells) {
+        BigDecimal numberOfGrain = new BigDecimal(1);
         for (int i = 0; i < numberOfCells; i++) {
             System.out.println(numberOfGrain);
-            numberOfGrain = (double) (numberOfGrain + Math.pow(2, i));
+            numberOfGrain =  numberOfGrain.add(new BigDecimal(Math.pow(2, i)));
 
         }
         return numberOfGrain;
